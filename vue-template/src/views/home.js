d9add7810d7079b58739380542b3bd5d61d0a49b
ShangJunLong <1472912024@qq.com>
2025-07-01 17:31:10 +0800

    vue模板使用

--- vue-template/src/views/home.js
+++ vue-template/src/views/home.js
@@ -1,12 +1,16 @@
-
 export default defineComponent({
     template: `
         <div>
             <h1>home 页面</h1>
+            <n-button @click="() => count++">count {{count}}</n-button>
         </div>
     `,
     setup() {
+        const count = ref(0)
 
+        return {
+            count
+        }
     }
 })
 
